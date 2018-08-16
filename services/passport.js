61cbbe16315f36885f665ecfb17b2c8f9e976c41
Joe Hinkley <joehhinkley@gmail.com>
2018-08-16 11:28:36 -0700

    Add heroku proxy tweaks

--- services/passport.js
+++ services/passport.js
@@ -21,7 +21,8 @@ passport.deserializeUser(async (id, done) => {
 passport.use(new GoogleStrategy({
     clientID: keys.googleClientID,
     clientSecret: keys.googleClientSecret,
-    callbackURL: "/auth/google/callback"
+    callbackURL: "/auth/google/callback",
+    proxy: true
 }, async (accessToken, refreshToken, profile, done) => {
     const existingUser = await User.findOne({ googleID: profile.id })
     if (existingUser) {
