a8661322b5e22c8e7a971d64e0e0254608fc7b8d
Joe Hinkley <joehhinkley@gmail.com>
2018-08-22 23:32:44 -0700

    Fix requestList name styling

--- client/src/components/RequestList.js
+++ client/src/components/RequestList.js
@@ -11,16 +11,16 @@ class RequestList extends Component {
                     key={request._id}
                     className="list-group-item" 
                 >
-                    <div className="row w-100">
+                    <div className="row">
                         <div className="col-3">
                             <img src={request.profileIMG} alt={request.fullName} className="rounded-circle" />
                         </div>
-                        <div className="col-9 align-self-center ">
-                            <label className="mb-0 pl-3">{request.fullName}</label>
+                        <div className="col-9 align-self-center">
+                            <label className="mb-0 pl-3 requestName">{request.fullName}</label>
                             <button 
                                 className="btn btn-success btn-sm ml-3"
                                 value={request._id}
-                                onClick={() => acceptRequest(request, user, socket)}
+                                onMouseDown={() => acceptRequest(request, user, socket)}
                             >
                                 Accept request
                             </button>
